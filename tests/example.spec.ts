a7d7424396c2e2cb046f4126d20fff4c24d1a4d6
gdum42 <gdumont42@live.fr>
2024-07-17 12:12:23 +0200

    add non-null assertion operator to remove errors in IDE

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,8 +3,8 @@ import { test, expect } from '@playwright/test'
 test.describe('testing Playwright', () => {
   test('has title', async ({ page }) => {
     await page.goto('/')
-    await page.locator('input[name=username]').fill(process.env.SIGNIN_USERNAME)
-    await page.locator('input[name=password]').fill(process.env.SIGNIN_PASSWORD)
+    await page.locator('input[name=username]').fill(process.env.SIGNIN_USERNAME!)
+    await page.locator('input[name=password]').fill(process.env.SIGNIN_PASSWORD!)
     await page.locator('input[type=submit]').click()
     await expect(page).toHaveTitle('ParaBank | Accounts Overview')
   })
